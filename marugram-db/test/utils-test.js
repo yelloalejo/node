65e60e69907a3a596a8b9555ae16bb1a6bb5ceeb
Diego Marulanda <quindialejo@gmail.com>
2017-04-03 19:58:35 -0500

    implementacion de Test

---
+++ marugram-db/test/utils-test.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const test = require('ava')
+const utils = require('../../lib/utils')
+
+test('extracting hashtags from text', t => {
+  let tags = utils.extractTags('a #picture with tags #AwEsomE #Platzi #AVA #100 ##yes')
+
+  t.deepEqual(tags, [
+    'picture',
+    'awesome',
+    'platzi',
+    'ava',
+    '100',
+    'yes'
+  ])
+
+  tags = utils.extractTags('a picture with no tags')
+  t.deepEqual(tags, [])
+
+  tags = utils.extractTags()
+  t.deepEqual(tags, [])
+
+  tags = utils.extractTags(null)
+  t.deepEqual(tags, [])
+})
