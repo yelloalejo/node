6cdcae0c52ddb099823d82ddabfb17ac578dac67
Diego Marulanda <quindialejo@gmail.com>
2017-05-12 14:07:31 -0500

    Fixture get image

--- marugram-db/test/db-test.js
+++ marugram-db/test/db-test.js
@@ -2,11 +2,30 @@
 
 const test = require('ava')
 const uuid = require('uuid-base62')
-const Db = require('../')
+const Db = require('../../lib/db')
 const r = require('rethinkdb')
+const fixtures = ('./fixtures')
 
 const dbName = `marugram_${uuid.v4()}`
 const db = new Db({ db: dbName })
+
+test('save image', async t => {
+  t.is(typeof db.saveImage, 'function', 'saveImage is a function')
+
+  let image = fixtures.getImage()
+
+  let created = await db.saveImage(image)
+
+  t.is(created.description, image.description)
+  t.is(created.url, image.url)
+  t.is(created.likes, image.likes)
+  t.is(created.liked, image.liked)
+  t.deepEqual(created.tags, [ 'awesome', 'tags', 'platzi' ])
+  t.is(created.user_id, image.user_id)
+  t.is(typeof created.id, 'string')
+  t.is(created.public_id, uuid.encode(created.id))
+  t.truthy(created.createAt)
+})
 
 test.before('setup database', async t => {
   await db.connect()
@@ -21,24 +40,4 @@ test.after('disconnect database', async t => {
 test.after.always('cleanup database', async t => {
   let conn = await r.connect({})
   await r.dbDrop(dbName).run(conn)
-})
-
-test('save image', async t => {
-  t.is(typeof db.saveImage, 'function', 'saveImage is a function')
-
-let image = {
-	url: 'http://marugram.test/${uuid.v4()}.jpg',
-	likes: 0,
-	liked: false,
-	user_id: uuid.uuid(),
-}
-
-let create = await db.saveImage(image)
-t.is(create.url, image.url)
-t.is(create.likes, image.likes)
-t.is(create.liked, image.liked)
-t.is(create.user_id, image.user_id)
-t.is(typeof create.id, 'string')
-t.truthy(create.createAt)
-
 })
