61b6a2c1aaba5834b30d9d4fea416ebf58c8ba77
Diego Marulanda <quindialejo@gmail.com>
2017-06-02 12:04:04 -0500

    Consulta de todas las imagenes de la base de datos

--- marugram-db/test/db-test.js
+++ marugram-db/test/db-test.js
@@ -4,18 +4,36 @@ const test = require('ava')
 const uuid = require('uuid-base62')
 const Db = require('../../lib/db')
 const r = require('rethinkdb')
-const fixtures = ('./fixtures')
+const fixtures = require('./fixtures')
 
-const dbName = `marugram_${uuid.v4()}`
-const db = new Db({ db: dbName })
+test.beforeEach('setup database', async t => {
+  const dbName = `marugram_${uuid.v4()}`
+  const db = new Db({ db: dbName })
+  await db.connect()
+  t.context.db = db
+  t.context.dbName = dbName
+  t.true(db.connected, 'should be connect')
+})
+
+test.afterEach.always('cleanup database', async t => {
+  let db = t.context.db
+  let dbName = t.context.dbName
+
+  await db.disconnect()
+  t.false(db.connected, 'should be disconnect')
+
+  let conn = await r.connect({})
+  await r.dbDrop(dbName).run(conn)
+})
 
 test('save image', async t => {
+  let db = t.context.db
+
   t.is(typeof db.saveImage, 'function', 'saveImage is a function')
 
   let image = fixtures.getImage()
 
   let created = await db.saveImage(image)
-
   t.is(created.description, image.description)
   t.is(created.url, image.url)
   t.is(created.likes, image.likes)
@@ -28,36 +46,34 @@ test('save image', async t => {
 })
 
 test('like image', async t => {
-  t.is(typeof db.likeImage, 'function', 'like image is a function')
+  let db = t.context.db
 
-  let image = fixtures.likeImage()
+  t.is(typeof db.likeImage, 'function', 'like image is a function')
+  let image = fixtures.getImage()
   let created = await db.saveImage(image)
   let result = await db.likeImage(created.public_id)
 
-  t.true(result.like, image.likes + 1)
-})
-
-test.before('setup database', async t => {
-  await db.connect()
-  t.true(db.connected, 'should be connect')
-})
-
-test.after('disconnect database', async t => {
-  await db.disconnect()
-  t.false(db.connected, 'should be disconnect')
-})
-
-test.after.always('cleanup database', async t => {
-  let conn = await r.connect({})
-  await r.dbDrop(dbName).run(conn)
+  t.true(result.liked)
+  t.is(result.like, image.likes + 1)
 })
 
 test('get image', async t => {
-  t.is(typeof db.getImage, 'function', get image is a function)
+  let db = t.context.db
+  t.is(typeof db.getImage, 'function', 'get image is a function')
 
   let image = fixtures.getImage()
   let created = await db.saveImage(image)
   let result = await db.getImage(created.public_id)
 
   t.deepEqual(created, result)
+})
+
+test('list all images', async t => {
+  let db = t.context.db
+  let images = fixtures.getImages(3)
+  let saveImages = images.map(img => db.saveImage(img))
+  let created = await Promise.all(saveImages)
+  let result = await db.getImage()
+
+  t.is(created.length, result.length)
 })
