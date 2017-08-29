886d2f344fa0354411d972cb9751f8094ccaeb16
Diego Marulanda <quindialejo@gmail.com>
2017-08-28 21:46:58 -0500

    fix error in test and encrypt password user

--- marugram-db/test/db-test.js
+++ marugram-db/test/db-test.js
@@ -5,6 +5,7 @@ const uuid = require('uuid-base62')
 const Db = require('../../lib/db')
 const r = require('rethinkdb')
 const fixtures = require('./fixtures')
+const utils = require('../../lib/utils')
 
 test.beforeEach('setup database', async t => {
   const dbName = `marugram_${uuid.v4()}`
@@ -76,4 +77,28 @@ test('list all images', async t => {
   let result = await db.getImage()
 
   t.is(created.length, result.length)
+})
+
+test('save user', async t => {
+  let db = t.context.db
+  t.is(typeof db.saveUser, 'function', 'saveUser is a function')
+  let user = fixtures.getUser()
+  let plainPassword = user.password
+  let created = await db.saveUser(user)
+  t.is(user.username, created.username)
+  t.is(user.email, created.email)
+  t.is(user.name, created.name)
+  t.is(utils.encrypt(plainPassword), created.password)
+  t.is(typeof created.id, 'string')
+  t.truthy(created.createdAt)
+})
+
+test('get user', async t => {
+  let db = t.context.db
+
+  t.is(typeof db.getUser, 'function', 'getUser is a function')
+  let user = fixtures.getUser()
+  let created = await db.saveUser(user)
+  let result = await db.getUser(user.username)
+  t.deepEqual(created, result)
 })
--- marugram-db/test/fixtures/index.js
+++ marugram-db/test/fixtures/index.js
@@ -1,25 +1,33 @@
-'use strict'
+    'use strict'
 
-const uuid = require('uuid-base62')
+    const uuid = require('uuid-base62')
 
-const fixtures = {
-  getImage () {
-    return {
-      description: 'an #awesome picture with #tags #platzi',
-      url: `http://marugram.test/${uuid.v4()}.jpg`,
-      likes: 0,
-      liked: false,
-      user_id: uuid.uuid()
+    const fixtures = {
+      getImage () {
+        return {
+          description: 'an #awesome picture with #tags #platzi',
+          url: `http://marugram.test/${uuid.v4()}.jpg`,
+          likes: 0,
+          liked: false,
+          user_id: uuid.uuid()
+        }
+      },
+      getImages (n) {
+        let images = []
+        while (n-- > 0) {
+          images.push(this.getImage())
+        }
+
+        return images
+      },
+      getUser () {
+        return {
+          name: 'A random user',
+          username: `user_${uuid.v4()}`,
+          password: uuid.uuid(),
+          email: `$(uuid.v4()}@platzi.test`
+        }
+      }
     }
-  },
-  getImages (n) {
-    let images = []
-    while (n-- > 0) {
-      images.push(this.getImage())
-    }
 
-    return images
-  }
-}
-
-module.exports = fixtures
+    module.exports = fixtures
